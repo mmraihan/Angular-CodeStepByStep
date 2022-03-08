e712a2780bb032fe7464f49036b40bf18ab62d15
mmraihan <mm_raihan@live.com>
2022-03-08 16:44:41 +0600

    Remove Unnecessary Components.

--- routing-blog/src/app/app.module.ts
+++ routing-blog/src/app/app.module.ts
@@ -3,22 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { UserComponent } from './user/user.component';
-import { NoPageComponent } from './no-page/no-page.component';
-import { AboutCompanyComponent } from './about-company/about-company.component';
-import { AboutMeComponent } from './about-me/about-me.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    AboutComponent,
-    UserComponent,
-    NoPageComponent,
-    AboutCompanyComponent,
-    AboutMeComponent
+    AppComponent
+    
   ],
   imports: [
     BrowserModule,
