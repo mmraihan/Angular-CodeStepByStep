336bb5151c9a930141ae2888332ac4915b001c5b
mmraihan <mm_raihan@live.com>
2022-03-08 15:33:34 +0600

    Wild Card.

--- routing-blog/src/app/app-routing.module.ts
+++ routing-blog/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { NoPageComponent } from './no-page/no-page.component';
 import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
@@ -16,6 +17,10 @@ const routes: Routes = [
   {
     component: HomeComponent,
     path:''
+  },
+  {
+    component: NoPageComponent,
+    path:'**'
   }
 ];
 
