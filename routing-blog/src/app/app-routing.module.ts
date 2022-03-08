12be7c57f1ee8b7677967ba569e46cb42e74b1b2
mmraihan <mm_raihan@live.com>
2022-03-08 16:36:20 +0600

    Child Routing.

--- routing-blog/src/app/app-routing.module.ts
+++ routing-blog/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutCompanyComponent } from './about-company/about-company.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { NoPageComponent } from './no-page/no-page.component';
@@ -8,7 +10,11 @@ import { UserComponent } from './user/user.component';
 const routes: Routes = [
   {
     component: AboutComponent,
-    path: 'about'
+    path: 'about',
+    children: [
+      {path: 'company', component: AboutCompanyComponent},
+      {path: 'me', component: AboutMeComponent}
+    ]
   },
   {
     component: UserComponent,
